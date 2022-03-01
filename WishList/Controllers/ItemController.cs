6fd14d77d674cb7fa617974c917081899f5ff085
Joseph Hilton <joeyhilton@kryogeniks.dev>
2022-02-28 23:21:45 -0500

    test

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -18,7 +18,7 @@ namespace WishList.Controllers
         }
         public IActionResult Index()
         {
-            return View(_context.Items.ToList());
+            return View(_context.Items);
         }
 
         [HttpGet]
